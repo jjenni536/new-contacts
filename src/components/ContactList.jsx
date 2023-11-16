4e7b2c65e0723b493a0cc093c7c02cc738d06305
justice jennings <justice.l.jennings@gmail.com>
2023-11-15 21:51:54 -0500

    added table to contact list file.

---
+++ src/components/ContactList.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import App from "../App";
+
+export default App
+const ContactList = () => {
+    return ( 
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="3">Contact List</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Name</td>
+              <td>Email</td>
+              <td>Phone</td>
+            </tr>
+            {
+               // Map over data here
+             }
+          </tbody>
+        </table>
+    ); 
+}
