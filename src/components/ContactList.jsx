54a7f22ca6b8517d0fdc247a57a9feea44d29cbc
justice jennings <justice.l.jennings@gmail.com>
2023-11-16 10:40:04 -0500

    added try/catch to fetch function.

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -3,25 +3,30 @@ import App from "../App";
 import { useState } from "react";
 import ContactRow from "./ContactRow";
 
+const dummyContacts = [
+  { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
+  { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
+  { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
+];
 
  const ContactList = () => {
-  
-const dummyContacts = [
-    { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
-    { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
-    { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
-  ];
+  const [contacts, setContacts] = useState([]);
 
-  useEffect(()=> {
-    const fetchContacts = Async () => {
-      const response = await fetch(
-        `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`
-        );
-        const data = await response.json();
-        console.log(data);
-        setContacts(data);
-    };
-    fetchContacts();
+  useEffect(() => {
+    try{
+      const fetchContacts = async () => {
+        const response = await fetch(
+          `https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`
+          );
+          const data = await response.json();
+          console.log(data);
+          setContacts(data);
+      };
+      fetchContacts();
+    }
+   catch(error) {
+    console.log(error);
+   }
   }, [];
 
     const [contacts, setContacts] = useState(dummyContacts)
